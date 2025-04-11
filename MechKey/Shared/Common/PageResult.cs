47f01d8044b41de03a7cbf252fcc28ddc3865399
Cong Thien <conggthien17@gmail.com>
2025-04-11 22:11:54 +0700

    BE-Add Shared Model

---
+++ MechKey/Shared/Common/PageResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Common
+{
+    public class PagedResult<T>
+    {
+        public IEnumerable<T> Items { get; set; } = Enumerable.Empty<T>();
+        public int TotalItems { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+    }
+}
