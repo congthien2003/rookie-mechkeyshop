d92335001fadaa87c564ea32628c75c14db65192
Cong Thien <conggthien17@gmail.com>
2025-04-11 22:11:37 +0700

    BE-Add base repository

---
+++ MechKey/Domain/Common/BaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Common
+{
+    public interface BaseRepository<T> where T : class
+    {
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<T> GetByIdAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+    }
+}
---
+++ MechKey/Infrastructure/Repositories/ApplicationUserRepository.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entity;
+using Domain.IRepositories;
+using MechkeyShop.Data;
+
+namespace Infrastructure.Repositories
+{
+    public class ApplicationUserRepository : IApplicationUserRepository<ApplicationUser>
+    {
+        private readonly ApplicationDbContext context;
+
+        public ApplicationUserRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<ApplicationUser> CreateAsync(ApplicationUser entity)
+        {
+            var newUser = context.ApplicationUsers.Add(entity);
+            await context.SaveChangesAsync();
+            return newUser.Entity;
+        }
+
+        public Task DeleteAsync(ApplicationUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ApplicationUser>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApplicationUser> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApplicationUser> UpdateAsync(ApplicationUser entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
