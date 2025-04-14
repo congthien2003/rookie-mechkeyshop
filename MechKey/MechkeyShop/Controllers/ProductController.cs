e1ac646e3c08d63945c01e8b38eba080c5ecaef8
Cong Thien <conggthien17@gmail.com>
2025-04-14 22:53:08 +0700

    MVC-Add Product Page, Product Detail Page

---
+++ MechKey/MechkeyShop/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Application.Interfaces.IServices;
+using MechkeyShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MechkeyShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService productService;
+        private readonly ICategoryService categoryService;
+
+        public ProductController(IProductService productService, ICategoryService categoryService)
+        {
+            this.productService = productService;
+            this.categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> IndexAsync(ProductPageViewModel model)
+        {
+            var resultListProduct = await productService.GetAllAsync(model.page, 4, model.searchTerm);
+            var resultListCategory = await categoryService.GetAllAsync(model.page, 10, "");
+            var totalPages = (int)Math.Ceiling(resultListProduct.Data.TotalItems / (double)resultListProduct.Data.PageSize);
+            if (resultListProduct.IsSuccess && resultListCategory.IsSuccess)
+            {
+                model.Products = resultListProduct.Data.Items;
+                model.Categories = resultListCategory.Data.Items;
+                model.page = resultListProduct.Data.Page;
+                model.pageSize = resultListProduct.Data.PageSize;
+                model.totalPages = totalPages;
+                model.searchTerm = model.searchTerm ?? "";
+                model.categoryId = model.categoryId ?? "";
+                return View(model);
+            }
+            return View();
+        }
+
+        [HttpGet("/Product/Detail/{id:guid}")]
+        public async Task<IActionResult> Detail(Guid id)
+        {
+            var result = await productService.GetByIdAsync(id);
+
+            if (!result.IsSuccess)
+            {
+                return View("Notfound");
+            }
+
+            var category = await categoryService.GetByIdAsync(result.Data.CategoryId);
+            ViewBag.CategoryName = category.Data.Name ?? "Unknown";
+
+            return View(result.Data);
+        }
+    }
+}
---
+++ MechKey/MechkeyShop/Models/ProductPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Shared.ViewModels;
+
+namespace MechkeyShop.Models
+{
+    public class ProductPageViewModel
+    {
+        public IEnumerable<CategoryModel> Categories { get; set; }
+        public IEnumerable<ProductModel> Products { get; set; }
+        public string categoryId { get; set; } = string.Empty;
+        public string searchTerm { get; set; } = string.Empty;
+        public int page { get; set; } = 1;
+        public int pageSize { get; set; } = 10;
+        public int totalCount { get; set; } = 0;
+        public int totalPages { get; set; } = 0;
+    }
+}
