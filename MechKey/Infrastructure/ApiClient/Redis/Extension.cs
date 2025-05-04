9b8dbe1d0c86b2c1317f910eadc5b53cb85c0930
Cong Thien <conggthien17@gmail.com>
2025-05-04 17:29:03 +0700

    BE - Add Redis Extension

---
+++ MechKey/Infrastructure/ApiClient/Redis/Extension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Infrastructure.ApiClient.Redis
+{
+    public static class Extension
+    {
+        public static IHostApplicationBuilder AddRedisCache(this IHostApplicationBuilder builder)
+        {
+            builder.Services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = builder.Configuration.GetSection("Redis:ConnectionString").Value;
+                options.InstanceName = "MechkeyShop-Redis";
+            });
+
+            return builder;
+        }
+    }
+}
