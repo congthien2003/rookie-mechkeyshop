7c305e4e9d2f1b268bc3df0979969eea7293904f
Cong Thien <conggthien17@gmail.com>
2025-05-04 17:29:19 +0700

    BE - Implement Redis/RedisService

---
+++ MechKey/Infrastructure/ApiClient/Redis/RedisService.cs
@@ -0,0 +1,42 @@
+﻿using Application.Interfaces.IApiClient.Redis;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+using System.Text.Json;
+
+namespace Infrastructure.ApiClient.Redis
+{
+    public class RedisService : IRedisService
+    {
+        private readonly IDistributedCache _cache;
+
+        public RedisService(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public T Get<T>(string key)
+        {
+            var data = _cache?.GetString(key);
+            if (data is null)
+            {
+                return default(T);
+            }
+            return JsonSerializer.Deserialize<T>(data);
+        }
+
+        public void Remove(string key)
+        {
+            _cache?.Remove(key);
+        }
+
+        public void Set<T>(string key, T value, int minutes)
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(minutes)
+            };
+            var data = JsonSerializer.Serialize<T>(value);
+            _cache?.SetString(key, data, options);
+        }
+    }
+}
