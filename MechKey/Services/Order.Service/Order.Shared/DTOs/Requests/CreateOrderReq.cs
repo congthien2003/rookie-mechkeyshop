07d4b073ceff0bfd3e76d817b435f02dd4be2294
nhoccuthien0538 <nhoccuthien0538@gmail.com>
2025-05-26 15:13:31 +0700

    Add DTOS

---
+++ MechKey/Services/Order.Service/Order.Shared/DTOs/Requests/CreateOrderReq.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Shared.DTOs.Requests
+{
+    public record class CreateOrderReq
+    {
+        public decimal TotalAmount { get; set; }
+        public Guid UserId { get; set; }
+        public List<OrderItemReq> orderItems { get; set; } = new List<OrderItemReq>();
+    }
+}
---
+++ MechKey/Services/Order.Service/Order.Shared/DTOs/Requests/OrderItemReq.cs
@@ -0,0 +1,10 @@
+﻿namespace Order.Shared.DTOs.Requests
+{
+    public class OrderItemReq
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
